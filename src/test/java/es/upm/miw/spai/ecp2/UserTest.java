2daf723bb743c9894e6a12ee0df92ed8c474df0d
magacoelho <mgacoelho@yahoo.es>
2014-10-08 01:04:19 +0200

    Agregado TestConstructor Clase User Constructor no está implementado Metodo getDomicilio agregado

--- src/test/java/es/upm/miw/spai/ecp2/UserTest.java
+++ src/test/java/es/upm/miw/spai/ecp2/UserTest.java
@@ -11,6 +11,8 @@ public class UserTest {
 	private static final String NAME_UPPER="Pepe";
 	private static final String FAMILY_NAME_UPPER="Pepes";
 	private static final String FAMILY_NAME="pepes";
+	private static final String ADDRESS="cll Soto 3";
+	private static final String ADDRESS_UPPER="Cll Soto 3";
 	@Before
 	 public void before() {
 		 user= new User(111, NAME, FAMILY_NAME);
@@ -30,5 +32,13 @@ public class UserTest {
     public void testInitials(){
 		 assertEquals(user.initials(),NAME.toUpperCase().substring(0, 1) + ".");
 	 }
-
+     @Test
+     public void testUserIntStringStrinString(){
+    	 User usuario = new User(111, NAME, FAMILY_NAME, ADDRESS);
+    	 assertEquals(111, usuario.getNumber());
+    	 assertEquals(NAME_UPPER, usuario.getName());
+    	 assertEquals(FAMILY_NAME_UPPER, usuario.getFamilyName());
+    	 assertEquals(ADDRESS_UPPER, usuario.getDomicilio());
+    	 
+     }
 }
